b19fd425b48394660bf7d935d17923a7fded9be6
Ben Gardner <ben.gardner@nwmotorsport.com>
2019-11-01 18:20:56 -0700

    Add: sorting by group

--- src/main/web.py
+++ src/main/web.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import sqlalchemy as db
 from collections import Counter
 from jinja2 import Template
+import operator
 
 
 def rename(var):
@@ -26,10 +27,12 @@ def home():
         print('Connected to DB')
     except Exception as e:
         print(e)
-    data = connection.execute("SELECT * FROM 'table';")
+    data = connection.execute("SELECT * FROM 'table' ORDER BY 'group' DESC;")
     data = data.fetchall()
+    data.sort(key= operator.itemgetter(6))
     locations = connection.execute("SELECT DISTINCT location FROM 'table';")
     locations = locations.fetchall()
+    locations.sort(key= operator.itemgetter(0))
     return render_template("home.html", data=data, locations=locations)
 
 if __name__ == "__main__":
